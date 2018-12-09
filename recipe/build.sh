63cb9a157a6878ebba07974db6074fb614de58ed
Matthew R Becker <becker.mr@gmail.com>
2018-12-09 09:54:15 -0600

    ENH add the cxx compiler too

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,4 +4,7 @@ export USE_CYTHON=True
 if [[ `uname` != "Darwin" ]] || [[ "$CC" != "clang" ]]; then
     export USE_OPENMP=True
 fi
-$PYTHON -m pip install . --no-deps -vv
+$PYTHON -m pip install . --no-deps -vv \
+    --global-option="build_ext" \
+    --global-option="-I${PREFIX}/include" \
+    --global-option="-L${PREFIX}/lib"
