8d65448859404d0be37627d24982a0ded0a0d064
Matthew R Becker <becker.mr@gmail.com>
2018-12-09 10:17:28 -0600

    MNT: Re-rendered with conda-smithy 3.1.12 and pinning 2018.11.27

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 export USE_CYTHON=True
-if [[ `uname` != "Darwin" ]] || [[ "$CC" != "clang" ]]; then
-    export USE_OPENMP=True
-fi
-$PYTHON -m pip install . --no-deps -vv \
-    --global-option="build_ext" \
-    --global-option="-I${PREFIX}/include" \
-    --global-option="-L${PREFIX}/lib"
+# if [[ `uname` != "Darwin" ]] || [[ "$CC" != "clang" ]]; then
+#     export USE_OPENMP=True
+# fi
+$PYTHON -m pip install . --no-deps -vv
+ # \
+ #    --global-option="build_ext" \
+ #    --global-option="-I${PREFIX}/include" \
+ #    --global-option="-L${PREFIX}/lib"
