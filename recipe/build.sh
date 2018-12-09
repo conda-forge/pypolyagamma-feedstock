5e611438c431eb1e8692041aa0c75ce88348253e
Matthew R Becker <becker.mr@gmail.com>
2018-12-09 13:23:12 -0600

    do not use cython

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-export USE_CYTHON=True
-
-CMD="$PYTHON -m pip install . --no-deps -vv --global-option=\"build_ext\""
-CMD="${CMD} --global-option=\"-I${PREFIX}/include\""
-CMD="${CMD} --global-option=\"-L${PREFIX}/lib\""
-CMD="${CMD} --global-option=\"-R${PREFIX}/lib\""
+CMD="$PYTHON -m pip install . --no-deps -vv "
 
 if [[ `uname` != "Darwin" ]] || [[ "$CC" != "clang" ]]; then
     export USE_OPENMP=True
-    CMD="${CMD} --global-option=\"-I${PREFIX}/lib/clang/4.0.1/include\""
+    if [[ `uname` == "Darwin" ]]; then
+        export CFLAGS="${CFLAGS} -I${PREFIX}/lib/clang/4.0.1/include"
+    fi
 fi
 
 ${CMD}
